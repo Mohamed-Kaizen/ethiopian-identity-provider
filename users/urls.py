8a2cc5a9a7b3ae15a85289d195fbffc8ca7d9e35
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-27 00:21:20 +0300

    add renew api end point

--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,22 @@
 from django.urls import include, path
 
 from .views import (
-    AcceptedBusinessAPI,
-    RequestedBusinessAPI,
-    UserInfoAPI,
-    UserProfileAPI,
+    OauthAcceptedBusinessAPI,
+    OauthRequestedBusinessAPI,
+    OauthUserInfoAPI,
+    OauthUserProfileAPI,
+    accepted_business_api,
+    renew_api,
+    requested_business_api,
 )
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
-    path("o/userinfo/", UserInfoAPI.as_view()),
-    path("o/profile/", UserProfileAPI.as_view()),
-    path("o/business/accepted/", AcceptedBusinessAPI.as_view()),
-    path("o/business/requested/", RequestedBusinessAPI.as_view()),
+    path("business/accepted/", accepted_business_api),
+    path("business/requested/", requested_business_api),
+    path("renew/", renew_api),
+    path("o/userinfo/", OauthUserInfoAPI.as_view()),
+    path("o/profile/", OauthUserProfileAPI.as_view()),
+    path("o/business/accepted/", OauthAcceptedBusinessAPI.as_view()),
+    path("o/business/requested/", OauthRequestedBusinessAPI.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 """Collection views."""
+from django.utils import timezone
 from oauth2_provider.contrib.rest_framework import TokenHasScope
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from .models import Renew
 from .serializers import (
     BusinessSerializer,
     UserInfoSerializer,
@@ -11,42 +14,42 @@ from .serializers import (
 )
 
 
-class UserInfoAPI(generics.GenericAPIView):
+class OauthUserInfoAPI(generics.GenericAPIView):
     """Oauth user info API."""
 
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     serializer_class = UserInfoSerializer
     required_scopes = ["user"]
 
-    def get(self: "UserInfoAPI", request: Request) -> Response:
+    def get(self: "OauthUserInfoAPI", request: Request) -> Response:
         """User info end point."""
         serializer = self.get_serializer(request.user)
 
         return Response(serializer.data)
 
 
-class UserProfileAPI(generics.GenericAPIView):
+class OauthUserProfileAPI(generics.GenericAPIView):
     """Oauth user profile API."""
 
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     serializer_class = UserProfileSerializer
     required_scopes = ["user:profile"]
 
-    def get(self: "UserProfileAPI", request: Request) -> Response:
+    def get(self: "OauthUserProfileAPI", request: Request) -> Response:
         """User profile end point."""
         serializer = self.get_serializer(request.user)
 
         return Response(serializer.data)
 
 
-class AcceptedBusinessAPI(generics.GenericAPIView):
+class OauthAcceptedBusinessAPI(generics.GenericAPIView):
     """Oauth accepted business API."""
 
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     serializer_class = BusinessSerializer
     required_scopes = ["business:accepted"]
 
-    def get(self: "AcceptedBusinessAPI", request: Request) -> Response:
+    def get(self: "OauthAcceptedBusinessAPI", request: Request) -> Response:
         """Accepted business end point."""
         businesses = request.user.businesses.filter(status="Accepted")
 
@@ -55,17 +58,56 @@ class AcceptedBusinessAPI(generics.GenericAPIView):
         return Response(serializer.data)
 
 
-class RequestedBusinessAPI(generics.GenericAPIView):
+class OauthRequestedBusinessAPI(generics.GenericAPIView):
     """Oauth requested business API."""
 
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
     serializer_class = BusinessSerializer
     required_scopes = ["business:requested"]
 
-    def get(self: "RequestedBusinessAPI", request: Request) -> Response:
+    def get(self: "OauthRequestedBusinessAPI", request: Request) -> Response:
         """Requested business end point."""
         businesses = request.user.businesses.filter(status="Requested")
 
         serializer = self.get_serializer(businesses, many=True)
 
         return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def accepted_business_api(request: Request) -> Response:
+    """Accepted business end point."""
+    businesses = request.user.businesses.filter(status="Accepted")
+
+    serializer = BusinessSerializer(businesses, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def requested_business_api(request: Request) -> Response:
+    """Requested business end point."""
+    businesses = request.user.businesses.filter(status="Requested")
+
+    serializer = BusinessSerializer(businesses, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def renew_api(request: Request) -> Response:
+    """Renew end point."""
+    if request.user.expired_at > timezone.now():
+        return Response(
+            {"detail": "Your account has not been expired yet"},
+            status.HTTP_400_BAD_REQUEST,
+        )
+
+    Renew.objects.create(user=request.user)
+
+    return Response(
+        {"detail": "You send renew request successfully"}, status.HTTP_201_CREATED
+    )
